6f0ffc3853925635cf047d6fb938890d24ea8cbb
Leŭ Pashkoŭski <LPashkouski@ibagroup.eu>
2022-02-16 15:45:51 +0300

    kek

--- app/src/main/java/com/levp/catsandmath/MainActivity.kt
+++ app/src/main/java/com/levp/catsandmath/MainActivity.kt
@@ -3,25 +3,18 @@ package com.levp.catsandmath
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.fragment.app.FragmentActivity
-import androidx.work.*
 import by.iba.rpms.presentation.common.navigation.RoutingParams
 import com.google.android.material.tabs.TabLayoutMediator
-import com.levp.catsandmath.core.ReminderWorker
 import com.levp.catsandmath.core.isOnline
 import com.levp.catsandmath.core.noInternetToast
 import com.levp.catsandmath.presentation.CatViewModel
 import com.levp.catsandmath.presentation.NFViewModel
 import com.levp.catsandmath.presentation.ViewPagerFragmentStateAdapter
-import com.levp.catsandmath.presentation.fragment.CatFragment
-import com.levp.catsandmath.presentation.navigation.Router
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.concurrent.TimeUnit
 
 
 class MainActivity : FragmentActivity() {
-
-    lateinit var router: Router
-
+    
     private val numFactNFViewModel: NFViewModel by viewModels()
 
     private val catViewModel: CatViewModel by viewModels()
